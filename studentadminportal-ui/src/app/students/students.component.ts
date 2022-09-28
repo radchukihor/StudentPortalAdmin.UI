8c9714a5584fd8cbd35e89f7373a086692e9e16c
Ihor Radchuk <90492848+radchukihor@users.noreply.github.com>
2022-09-28 13:30:41 +0800

    added models ui

--- studentadminportal-ui/src/app/students/students.component.ts
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import { Student } from '../models/ui-models/student.model';
 import { StudentService } from './student.service';
 
 @Component({
@@ -7,13 +9,24 @@ import { StudentService } from './student.service';
   styleUrls: ['./students.component.css'],
 })
 export class StudentsComponent implements OnInit {
+  students: Student[] = [];
+  displayedColumns: string[] = [
+    'firstName',
+    'lastName',
+    'dateOfBirth',
+    'email',
+    'mobile',
+    'gender',
+  ];
+
+  dataSource: MatTableDataSource<Student> = new MatTableDataSource();
   constructor(private studentServiice: StudentService) {}
 
   ngOnInit(): void {
     this.studentServiice.getStudent().subscribe(
       (successResponse) => {
-        console.log(successResponse[0].firstName);
-        console.log(successResponse[0].lastName);
+        this.students = successResponse;
+        this.dataSource = new MatTableDataSource<Student>(this.students);
       },
       (errorResponse) => {
         console.log(errorResponse);
