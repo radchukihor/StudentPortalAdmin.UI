01f18c33b2dd5b694382d6d8627ab09f76c11c07
Ihor Radchuk <90492848+radchukihor@users.noreply.github.com>
2022-09-28 19:09:28 +0800

    Added sorting on the table

--- studentadminportal-ui/src/app/students/students.component.ts
+++ studentadminportal-ui/src/app/students/students.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
 import { MatTableDataSource } from '@angular/material/table';
 import { Student } from '../models/ui-models/student.model';
 import { StudentService } from './student.service';
@@ -22,6 +23,7 @@ export class StudentsComponent implements OnInit {
 
   dataSource: MatTableDataSource<Student> = new MatTableDataSource();
   @ViewChild(MatPaginator) matPaginator!: MatPaginator;
+  @ViewChild(MatSort) matSort!: MatSort;
   constructor(private studentServiice: StudentService) {}
 
   ngOnInit(): void {
@@ -32,6 +34,10 @@ export class StudentsComponent implements OnInit {
 
         if (this.matPaginator) {
           this.dataSource.paginator = this.matPaginator;
+        }
+
+        if (this.matSort) {
+          this.dataSource.sort = this.matSort;
         }
       },
       (errorResponse) => {
